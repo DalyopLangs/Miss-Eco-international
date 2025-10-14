fd6629b09f16e384d2a12ab852894cf59d2a23b4
DalyopLangs <dalyoplangs2022@gmail.com>
2025-10-14 03:08:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-
-function toggleMenu() {
-    document.getElementById("nav-links").classList.toggle("active");
-}
-
-
+‎const hamburgerMenu = document.querySelector(".hamburger-menu");
+‎const navLinks = document.querySelector("#nav-links");
+‎
+‎hamburgerMenu.addEventListener("click", () => {
+‎  navLinks.classList.toggle("active");
+‎})
